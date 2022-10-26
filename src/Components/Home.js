ec898eb6a8275b8277c2c851bb9aa02ecffe8559
aridguy <rotimiariyo@gmail.com>
2022-10-26 20:23:22 +0100

    added the stack and twitter icons, meanwhile the slack icon has a display of hidden

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 import Pics from '../Assets/profile_pics.jpg';
+import Twitter from '../Assets/twitter.png';
+import Slack from '../Assets/slack.svg';
 
 import './Home.css'
 
@@ -16,6 +18,8 @@ const Home = () => {
                         <div className="overlay">
                             <img className="img-round mt-5 " id="profile_img" src={Pics} />
                             <h3 className="name">Idowu Ariyo</h3>
+                            <span className="fa fa-twitter twitter"></span>
+                           <img className="slack" src={Slack} />
                         </div>
                     </div>
                     <div className="col-md-4">
@@ -27,6 +31,7 @@ const Home = () => {
                 <div className="row">
                     <div className="col-md-12">
                         <div className="contents-links">
+                        
                         </div>
                     </div>
                 </div>
